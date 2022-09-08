a0d59ccdcbd4f69a7d9b1ceb22dfcaf9690bca78
tefsis <tefsis@gmail.com>
2022-09-08 07:31:51 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
-#include <stdio.h>                                                                                               
-                                                                                                                 
-/**                                                                                                              
- * main - Entry point                                                                                            
- *                                                                                                               
- * Return: Always 0 (Success)                                                                                    
- */                                                                                                              
-int main(void)                                                                                                   
-{
-  int i;
-  char c;
-  float f;
-  long int li;
-  long long int lli;
-  printf("size of a char:%zu byte(s)\n",sizeof(c)); 
-  printf("size of an int:%zu byte(s)\n",sizeof(i));
-  printf("size of a long int:%zu byte(s)\n",sizeof(li));
-  printf("size of a long long int:%zu byte(s)\n",sizeof(lli));
-  printf("size of a float:%zu byte(s)\n",sizeof(f));  
-return (0);
+#include <stdio.h>                                                                                                 
+                                                                                                                   
+/**                                                                                                                
+ * main - Entry point                                                                                              
+ *                                                                                                                 
+ * Return: Always 0 (Success)                                                                                      
+ */                                                                                                                
+int main(void)                                                                                                     
+{                                                                                                                  
+  int i;                                                                                                          
+  char c;                                                                                                         
+  float f;                                                                                                  
+  long int li;                                                                  
+  long long int lli;                                                               
+  printf("size of a char: %zu byte(s)\n",sizeof(c));                                                     
+  printf("size of an int: %zu byte(s)\n",sizeof(i));                                                          
+  printf("size of a long int: %zu byte(s)\n",sizeof(li));                                                
+  printf("size of a long long int: %zu byte(s)\n",sizeof(lli));                                                   
+  printf("size of a float: %zu byte(s)\n",sizeof(f));                                                          
+  return (0);                                                                                             
 }
