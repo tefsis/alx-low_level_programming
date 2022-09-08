e8116748d1cda7195b02d01a19c494528b6f9838
tefsis <tefsis@gmail.com>
2022-09-08 05:51:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  char hello []= ""Programming is like building a multilingual puzzle";
-puts(hello);
-  return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
