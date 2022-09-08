43c0078ba9ff80b2022fcf7cc5ca32b8c805bc55
tefsis <tefsis@gmail.com>
2022-09-08 06:33:39 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,7 @@
-#include <stdio.h>                                                                                               
-                                                                                                                 
-/**                                                                                                              
- * main - Entry point                                                                                            
- *                                                                                                               
- * Return: Always 0 (Success)                                                                                    
- */                                                                                                              
+#include <stdio.h>
+                                                                                                             
 int main(void)                                                                                                   
-{                                                                                                               printf("with proper grammar, but the outcome is a piece of art,/n");
+{                                                                                                               printf("with proper grammar, but the outcome is a piece of art,"/n);
 
 return (0);                                                                                                      
 }                                  
