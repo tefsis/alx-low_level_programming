ad6245fa66d163de1ea1a82dbde26bcc04bfce17
tefsis <tefsis@gmail.com>
2022-09-09 08:34:02 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,19 @@
  */
 int main(void)
 {
-int i, j;
-for (i = 0; i <= 9; i++)
-{
-for (j = i; j <= 9; j++)
-{
-if (j == i)
-continue;
-putchar((i % 10) + '0');
-putchar((j % 10) + '0');
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+  int i, j;
+  for (i = 0; i <= 9; i++)
+    {
+      for (j = i; j <= 9; j++)
+	{
+	  if (j == i)
+	    continue;
+	  putchar((i % 10) + '0');
+	  putchar((j % 10) + '0');
+	  putchar(',');
+	  putchar(' ');
+	}
+    }
+  putchar('\n');
+  return (0);
 }
