a2291f8349d633f08beb3afae79282ab96afa426
tefsis <tefsis@gmail.com>
2022-09-09 05:48:10 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+char chr;
+int i;
+for (i = 0 ; i <= 10; i++)
+putchar(i % 10 ) + '0');
+for (chr = 'a'; chr <= 'f'; chr++)
+putchar(chr);
+putchar('\n');
+return (0);
+}
