8ecb8f81f5b2c8d1cc8fd0385d92f5bc08b5f572
tefsis <tefsis@gmail.com>
2022-09-09 12:54:51 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,21 +8,23 @@
  */
 int main(void)
 {
-  int i,j,k;
-  for (i = 0; i <= 9; i++)
-    {
-      for (j = i+1; j <= 9; j++)
-	{
-	  for (k = j+1; k <= 9; k++)
-	    {
-	   	  putchar((i % 10) + '0');
-	  putchar((j % 10) + '0');
-	  putchar((k % 10) + '0'); 
-	  putchar(',');
-	  putchar(' ');
-	}
-	}
-    }
-  putchar('\n');
-  return (0);
+int i,j,k;
+for (i = 0; i <= 9; i++)
+{
+for (j = i+1; j <= 9; j++)
+{
+for (k = j+1; k <= 9; k++)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+putchar((k % 10) + '0');
+if (i == 7 && j == 8 && k == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
