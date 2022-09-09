d3bbcc239a5e8bad85d9b58f10a27c5f0cf6a626
tefsis <tefsis@gmail.com>
2022-09-09 07:16:46 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,13 +15,15 @@ for (i = 0; i <= 9; i++)
 putchar((i % 10) + '0');
 if (i == 9)
 continue;
-for (j = i; j <= 9  && j != i; j++)
+ {
+ for (j = i; j <= 9  && j != i; j++)
 putchar((j % 10) + '0');
 if (j == 9)
 continue;
 putchar(',');
 putchar(' ');
 }
-putchar('\n');
+ }
+ putchar('\n');
 return (0);
 }
