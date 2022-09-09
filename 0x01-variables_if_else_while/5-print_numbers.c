5c398af53320f0fa58e4a791c837d452108f0b7c
tefsis <tefsis@gmail.com>
2022-09-09 05:09:02 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+int i;
+for (i=0; i<10; i++)
+printf(i'\n');
+return (0);
+}
