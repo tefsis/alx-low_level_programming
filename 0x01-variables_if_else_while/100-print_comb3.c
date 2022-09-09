ee0e9e9f3e7f1bbb16e8bfb96977648b755ab8cf
tefsis <tefsis@gmail.com>
2022-09-09 06:51:53 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+int i,j;
+for (i = 0; i <= 9; i++)
+putchar((i % 10) + '0');
+{
+for (j = i; i <= 9  && j != i; j++)
+putchar((j % 10) + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
