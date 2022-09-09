bf36ee6e1dad7d9fa54c0dea0981d76779156196
tefsis <tefsis@gmail.com>
2022-09-09 04:45:10 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+char chr;
+for (chr = 'a'; chr <= 'z'; chr++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
