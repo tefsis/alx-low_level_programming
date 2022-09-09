3ee218325592dceb23842f3d34198f99bf7cb18a
tefsis <tefsis@gmail.com>
2022-09-09 04:50:02 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+char chr;
+for (chr = 'a'; chr <= 'z'; chr++)
+putchar(chr);
+for (chr = 'A'; chr <= 'Z'; chr++)
+putchar(chr);
+putchar('\n');
+return (0);
+}
