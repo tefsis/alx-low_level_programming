1dca34f90d18d80a430e9b75bc7feb07fbf47a4a
tefsis <tefsis@gmail.com>
2022-09-09 04:57:29 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+char chr;
+for (chr = 'a'; chr <= 'z'; chr++)
+if ( Char != 'q' && char != 'e')
+putchar(chr);
+putchar('\n');
+return (0);
+}
