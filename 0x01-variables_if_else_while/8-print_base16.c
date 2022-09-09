49213941b00f49bec403e7f07efeae48775c04fc
tefsis <tefsis@gmail.com>
2022-09-09 05:54:50 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,8 +10,8 @@ int main(void)
 {
 char chr;
 int i;
-for (i = 0 ; i <= 10; i++)
-putchar((i % 10 )+'0');
+for (i = 0 ; i < 10; i++)
+putchar((i % 10) + '0');
 for (chr = 'a'; chr <= 'f'; chr++)
 putchar(chr);
 putchar('\n');
