8891c697467b54ff1d3b71aa00662ed0b56f9d7e
tefsis <tefsis@gmail.com>
2022-09-09 08:04:11 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+  int i,j,k;
+  for (i = 0; i <= 9; i++)
+    {
+      for (j = i; j <= 9; j++)
+	{
+	  for (k = j; k <= 9; i++)
+	    {
+	    if (j == i == k)
+	    continue;
+	  putchar((i % 10) + '0');
+	  putchar((j % 10) + '0');
+	  putchar((k % 10) + '0'); 
+	  putchar(',');
+	  putchar(' ');
+	}
+	}
+    }
+  putchar('\n');
+  return (0);
+}
