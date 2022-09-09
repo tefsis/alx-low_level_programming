9fe1d44158f134ae487229357a7935a445993ff5
tefsis <tefsis@gmail.com>
2022-09-09 05:23:30 -0700

    6-print_numberz.c root@42f167e01367:/alx-low_level_programming/0x01-variables_if_else_while#

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+int chr;
+for (chr = 0; chr < 10; chr++)
+putchar(chr);
+putchar('\n');
+return (0);
+}
