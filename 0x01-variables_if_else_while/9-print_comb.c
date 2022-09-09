cb310fd779d6cf50ea29510a13d5d5cf55590876
tefsis <tefsis@gmail.com>
2022-09-09 06:06:42 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+int i,j;
+for (i = 0; i <=9; i++)
+{
+putchar(i % 10) + '0');
+for (j = 0; j <= i; j++)
+putchar(',');
+putchar(' ');
+return (0);
+}
