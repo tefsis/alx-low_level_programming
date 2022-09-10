9590ec14ccddab2c9cdf68e3d8b61f8ec1315945
tefsis <tefsis@gmail.com>
2022-09-10 07:30:40 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 int num1, num2;
- for (num1 = 0; num1 <= 98; num1++)
+for (num1 = 0; num1 <= 98; num1++)
 {
 for (num2 = num1 + 1; num2 <= 99; num2++)
 {
@@ -16,7 +16,7 @@ putchar((num1 % 10) + '0');
 putchar(' ');
 putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
-if (num1 == 98 && num2 == 99 )
+if (num1 == 98 && num2 == 99)
 continue;
 putchar(',');
 putchar(' ');
