cd36c98cc4341909f1433eef461117f18285c9d1
tefsis <tefsis@gmail.com>
2022-09-13 05:42:56 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,13 +11,13 @@ void print_alphabet_x10(void)
   int i;
 
   for (i = 0; i <= 9; i++)
-    {
+  
       for (chr = 'a'; chr <= 'z'; chr++)
 	{
 	  _putchar(chr);
 	  _putchar('\n'); 
 	}
     
-      _putchar('\n');
-    }
+      
+    
 }
