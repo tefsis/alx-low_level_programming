d7d6477f3e950403b7d8fb9cfa29147ccc5a19f9
tefsis <tefsis@gmail.com>
2022-09-13 01:34:09 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+void print_alphabet(void);
+{
+char chr;
+for (chr = 'a'; chr <= 'z'; chr++)
+putchar(chr);
+putchar('\n');
+}
+return (0);
+}
