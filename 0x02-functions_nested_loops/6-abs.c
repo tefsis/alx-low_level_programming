498022475c16ebc6aa19009473afcd0d313deea5
tefsis <tefsis@gmail.com>
2022-09-13 07:18:10 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,14 +6,13 @@
  */
 int _abs(int i)
 {
-  if (i >= 0)
-    {
-    return (i);
-
-    }
-  else if (i < 0)
-    {
-    return (-i);
-    }
+if (i >= 0)
+{
+return (i);
+}
+else if (i < 0)
+{
+return (-i);
+}
 return (0);
 }
