8f0a6c6c07474fa6fb6171f5fd95f07244ead1de
tefsis <tefsis@gmail.com>
2022-09-13 07:09:55 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - Prints the absolute value of  a number
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int)
+{
+  int i;
+  if (i >= 0)
+    return (i);
+  else if (i < 0)
+    return (-i)
+}
