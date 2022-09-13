41d45f3fcbac259b8a32b24ae9a259aec634c1bd
tefsis <tefsis@gmail.com>
2022-09-13 12:38:14 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,18 +8,33 @@
 
 void times_table(void)
 {
-  int i;
-  int j;
-  int k;
-  for (i = 0; i <= 9; i++)
+  int x, y, product;
+
+  for (x = 0; x <= 9; x++)
     {
-      for (j = 0; j <= 9; j++)
-	k = i*j;
-      _putchar(k + '0');
-      if (j == 9)
-	continue;
-	  _putchar(',');
-      _putchar(' ');
+      for (y = 0; y <= 9; y++)
+	{
+	  product = x * y;
+	  if (y != 0)
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	    }
+	  if (y == 0)
+	    {
+	      _putchar('0');
+	    }
+	  else if (product >= 10)
+	    {
+	      _putchar((product / 10) + '0');
+	      _putchar((product % 10) + '0');
+	    }
+	  else if ((product < 10) && (y != 0))
+	    {
+	      _putchar(' ');
+	      _putchar((product % 10) + '0');
+	    }
+	}
+      _putchar('\n');
     }
-  _putchar('\n');
 }
