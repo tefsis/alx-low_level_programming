261cc6cce0840cc8b83f281e4c409d476a25cfa1
tefsis <tefsis@gmail.com>
2022-09-13 06:53:05 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,16 @@ int print_sign(int n)
 {
   if (n > 0)
     {
-    putchar('+');
+  
     return (1);
+    putchar('+');
     }
-  else if( n > 0)                                                                                                  {     
-      putchar('0');                                                                                                   
-return (0);
+  else if( n == 0)                                                                                                  {     
+     return (0);
+      putchar('0'); 
   }
  else
-   putchar('-');
+   
     return (-1);
+    putchar('-'); 
 }
