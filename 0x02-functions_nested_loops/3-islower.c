ead73e7a1a6c95b9a7a8fb3cddc7e84af221c5da
tefsis <tefsis@gmail.com>
2022-09-13 05:36:02 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+  int c;
+  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    return (1)
+    else
+      return (0)
+	}
