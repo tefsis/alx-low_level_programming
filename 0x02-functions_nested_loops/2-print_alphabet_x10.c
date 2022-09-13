6b015fac39bece35c1859e8ed09a509cf7ea97d8
tefsis <tefsis@gmail.com>
2022-09-13 01:47:11 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+void print_alphabet_x10(void);
+{
+  char chr;
+  int i;
+ for (i = 0; i <= 9; i++)
+{
+for (chr = 'a'; chr <= 'z'; chr++)
+  putchar(chr);
+ putchar('\n');
+ }
+}
+int main(void)
+{
+return (0);
+}
