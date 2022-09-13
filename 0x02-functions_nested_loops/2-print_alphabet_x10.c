ad350bc3375d2aa6edf6a809966e451fbd570cae
tefsis <tefsis@gmail.com>
2022-09-13 05:14:02 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,12 @@
  */
 void print_alphabet_x10(void)
 {
-  int i;
-  for (i =0; i <= 9; i++)
-    {
-  char alpha;
-  for (alpha = 'a'; alpha <= 'z'; alpha++)
-    
-      _putchar(alpha);
-    }
-    
-  _putchar('\n');
+int i;
+for (i =0; i <= 9; i++)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+_putchar(alpha);
+_putchar('\n'0;
+}
 }
