db14df41c5aac0e260bf7fb295deeff2bbe8c690
tefsis <tefsis@gmail.com>
2022-09-13 11:57:32 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,13 +4,12 @@
  *
  * Return: Always 0 (Success)
  */
- int print_last_digit(int i)
- {
- {
- i = (i % 10);
- if (i < 0)
- i = -i;
- _putchar(i + '0');
- }
+int print_last_digit(int i)
+{
+i = (i % 10);
+if (i < 0)
+  i = -i;
+_putchar(i + '0');
+}
 return (i);
- }
+}
