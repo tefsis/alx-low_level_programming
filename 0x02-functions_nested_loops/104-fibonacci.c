8816f7533e75118640f87a84738721dddb422ef1
tefsis <tefsis@gmail.com>
2022-09-14 08:30:18 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+  unsigned long i;
+  unsigned long i1 = 1;
+  unsigned long i2 = 2;
+  unsigned long k;
+  printf("%lu, %lu, ", i1, i2);
+  for (i = 3; i <= 98; i++)
+    {
+      k = i1 + i2;
+      printf("%lu", k);
+      i1 = i2;
+      i2 = k;
+      if (i == 50)
+	printf("\n");
+      else
+	printf(", ");
+    }
+  return (0);
+}
