7caa4870ac407bc75a74c8f162a960d272c93e3f
tefsis <tefsis@gmail.com>
2022-09-14 05:09:31 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
   int i;
   int j;
   int k;
-  if ( n > 0 && n < 15)
+  while ( n >= 0 && n < 15)
     {
   for (i = 0; i <= n; i++)
     {
@@ -23,7 +23,7 @@ void print_times_table(int n)
  _putchar(',');
    _putchar(' '); 
   	    }	    
-	  else if (k > 9){
+	  else {
 	    _putchar((k / 10) + '0');                                                                                
           _putchar((k % 10) + '0');                                                                                
           _putchar(',');                                                                                           
