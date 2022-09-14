5d51faca2a29ae11d0d8d1b208f525dc1553dcb0
tefsis <tefsis@gmail.com>
2022-09-14 05:56:57 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+int i;
+int k = 0;
+for (i = 0; i < 1024; i++)
+  {
+    if ((i % 3 == 0) || (i % 5 == 0))
+      {
+	k = k + i;
+	printf("%d",k);
+	printf("\n");
+      }
+  }
+    return (0);
+}
