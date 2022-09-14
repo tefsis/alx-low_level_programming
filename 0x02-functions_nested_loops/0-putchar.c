f9f2fb6c3024e6bfe6f454ad1ec61b100469b2b6
tefsis <tefsis@gmail.com>
2022-09-14 04:17:21 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _putchar - writes the character c to stdout
+ * main - writes the character c to stdout
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
