8912506f4ea18f0801aa2ed25cfb97d3a3e237c5
tefsis <tefsis@gmail.com>
2022-09-14 06:33:28 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints the various sizes of data
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+int i;
+int k;
+int i1 = 1;
+int i2 = 2;
+k = i1 + 12;
+printf("%d, %d, ", t1, t2);
+for (i = 3; i <= n; ++i) {
+printf("%d, ", k);
+i1 = i2;
+i2 = k;
+ k = i1 + i2;
+}
+return 0;
+}
