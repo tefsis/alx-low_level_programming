2f49088ea064309197a7a6731bf9b6f4b46fe6b7
tefsis <tefsis@gmail.com>
2022-09-14 04:43:05 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints an n by n time table
+ *@n:the size of the time table
+ * Return: Always 0.
+ */
+void print_times_table(int n)
+{
+  int i;
+  int j;
+  int k;
+  if ( n > 0 && n < 15)
+    {
+  for (i = 0; i <= n; i++)
+    {
+      for (j = 0; j < n; j++)
+	{
+	  k = i*j;
+	  if(k > 9)
+	    {
+	  _putchar((k / 10) + '0');
+	  _putchar((k % 10) + '0');
+	  _putchar(',');
+	  _putchar(' ');
+	    }
+	  else
+	    putchar(k);                                                                              
+          _putchar(',');                                                                                         
+          _putchar(' ')
+	    }
+	}
+    }
+
+}
