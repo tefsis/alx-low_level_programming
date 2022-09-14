206222af8b166fc9bf2c2353250b24d4850af912
tefsis <tefsis@gmail.com>
2022-09-14 06:40:54 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,11 +14,12 @@ int i1 = 1;
 int i2 = 2;
 k = i1 + i2;
 printf("%d, %d, ", i1, i2);
-for (i = 3; i <= 50; ++i) {
+for (i = 3; i <= 50; ++i)
+{
 printf("%d, ", k);
 i1 = i2;
 i2 = k;
 k = i1 + i2;
 }
-return 0;
+return (0);
 }
