6d41f88edf699aca5b77c2427b7f7b8c057b48d4
tefsis <tefsis@gmail.com>
2022-09-14 05:23:25 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,17 +9,42 @@ void print_times_table(int n)
 {
   int i;
   int j;
-  int k;
-  while ( n >= 0 && n < 15)
+  if (n >= 0 && n <= 15)
     {
-  for (i = 0; i <= n; i++)
-    {
-      for (j = 0; j < n; j++)
+      for (i = 0; i <= n; i++)
 	{
-	  printf("%dx%d = %d, ",j,i,i*j);
+	  for (j = 0; j <= n; j++)
+	    {
+	      if ((i * j) < 10)
+		{
+		  if (j != 0)
+		    {
+		      _putchar(' ');
+		      _putchar(' ');
+		      _putchar(' ');
+		    }
+		  _putchar((j * i) + '0');
+		}
+	      else if ((i * j) < 100)
+		{
+		  _putchar(' ');
+		  _putchar(' ');
+		  _putchar((((j * i) / 10) % 10) + '0');
+		  _putchar(((j * i) % 10) + '0');
+		}
+	      else if ((i * j) < 1000)
+		{
+		  _putchar(' ');
+		  _putchar((((j * i) / 100) % 10) + '0');
+		  _putchar((((j * i) / 10) % 10) + '0');
+		  _putchar(((j * i) % 10) + '0');
+		}
+	      if (j != (n))
+		{
+		  _putchar(',');
+		}
+	    }
+	  _putchar('\n');
 	}
-        }
-
-}
-  printf("\n");
+    }
 }
