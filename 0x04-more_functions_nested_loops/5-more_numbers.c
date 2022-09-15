d3bb0b54422e37604eb9a94f02176272f9d34186
tefsis <tefsis@gmail.com>
2022-09-15 06:32:48 -0700

    print numbers 1-14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,16 @@ int i;
 int j;
 for (i = 0; i <= 10; i++)
 {
-for (j = 0; j <= 14; j++)
-{
-if (j >= 10)
-{
-_putchar(j / 10 + '0');
-}
-_putchar('0' + j);
-}
-_putchar('\n');
-}
+  j = 0;
+  while (j <= 14)
+    {
+      if (j >= 10)
+	{
+	  _putchar(j / 10 + '0');
+	}
+      _putchar(j % 10 + '0');
+      j++;
+    }
+  _putchar('\n');
+ }
 }
