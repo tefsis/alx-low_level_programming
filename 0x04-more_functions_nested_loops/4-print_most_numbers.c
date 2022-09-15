b6cd9413150e67def2a8621b1a5f8460c3888d4c
tefsis <tefsis@gmail.com>
2022-09-15 09:53:11 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,14 @@ void print_most_numbers(void);
 int i;
 for (i = 0; i < 10; i++)
 {
-if ((i == 2) && (i == 4))
+if ((i == 2) || (i == 4))
+{
 continue;
+}
+else
+{
 _putchar('0' + i);
+}
 }
 _putchar('\n');
 }
