dd045e65908663032df411f09179c95c3935d460
tefsis <tefsis@gmail.com>
2022-09-15 11:11:45 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,9 +7,13 @@ void print_diagonal(int n)
   int i;
   int j;
   for (j = 1; j < n; j++)
+    {
     _putchar(' ');
   _putchar('\n');
+    }
   for (i = 0; i < n; i++)
+    {
     _putchar('\\');
   _putchar('\n');
+}
 }
