b9864954fc464fdba418a87ef71f897de10ea983
tefsis <tefsis@gmail.com>
2022-09-15 11:48:19 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_square - prints square followed by new line
+ *@size: square size
+ */
+void print_square(int size);
+{
+  int i;
+  int j;
+  while (n > 0)
+    {
+      for (i = 1; i < = n; i++)
+	{
+
+	  for (j = 0; j < = n; j++)
+	    {
+
+	      _putchar('#');
+
+	    }
+	   _putchar('\n');
+	}
+    }
+  _putchar('\n');
+  return (0);
+
+}
