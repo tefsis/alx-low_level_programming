b9b6f524e28f8364530257f877e2b7c0f5a74133
tefsis <tefsis@gmail.com>
2022-09-15 01:23:25 -0700

    my first debbuging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,15 +7,16 @@
 
 int main(void)
 {
-int i;
-printf("Infinite loop incoming :(\n");
+  int i;
 
-i = 0;
+  printf("Infinite loop incoming :(\n");
+
+  i = 0;
 /**
- *while (i < 10)
- *{
- *putchar(i);
- *}
+ * while (i < 10)
+ * {
+ putchar(i);
+ * }
  */
 printf("Infinite loop avoided! \\o/\n");
 
