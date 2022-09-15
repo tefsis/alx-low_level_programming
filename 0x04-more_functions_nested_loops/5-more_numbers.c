b4d427c11e709dbd9d000a27d4b470a8b3695ffe
tefsis <tefsis@gmail.com>
2022-09-15 10:11:52 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,19 +4,19 @@
  */
 void more_numbers(void)
 {
-  int x, y;
-  for (y = 0; y < 10; y++)
-    {
-      x = 0;
-      while (x <= 14)
-	{
-	  if (x >= 10)
-	    {
-	      _putchar(x / 10 + '0');
-	    }
-	  _putchar(x % 10 + '0');
-	  x++;
-	}
-      _putchar('\n');
-    }
+int x, y;
+for (y = 0; y < 10; y++)
+{
+x = 0;
+while (x <= 14)
+{
+if (x >= 10)
+{
+_putchar(x / 10 + '0');
+}
+_putchar(x % 10 + '0');
+x++;
+}
+_putchar('\n');
+}
 }
