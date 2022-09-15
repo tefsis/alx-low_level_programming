16045b11dd7dbf034d5d4ce03f67988167887cfa
tefsis <tefsis@gmail.com>
2022-09-15 06:01:30 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_most_numbers - prints number 0-9 exept 2 & 4 followed by newline
+ */
+void print_most_numbers(void);
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if ((i == 2) && (i == 4))
+continue;
+_putchar('0' + i);
+}
+_putchar('\n');
+}
