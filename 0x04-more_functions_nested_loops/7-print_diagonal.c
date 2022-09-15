06dbfcc096f3a1377631b565f200598110da1326
tefsis <tefsis@gmail.com>
2022-09-15 07:28:59 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_diagonal - prints diagonal followed by newline
+ */
+void print_diagonal(int n)
+{
+  int i;
+  int j;
+  for (j = 1; j < n; j++)
+    _putchar(' ');
+  putchar('\n');
+  for (i = 0; i < n; i++)
+    _putchar('\');
+  _putchar('\n');
+}
