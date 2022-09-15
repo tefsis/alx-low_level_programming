af89a60b3abb43cdf547df0cfe88f3c0d9f72d2d
tefsis <tefsis@gmail.com>
2022-09-15 05:36:21 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_numbers - prints number 0-9 followed by newline
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar("%d" , i);
+}
+_putchar('\n');
+}
