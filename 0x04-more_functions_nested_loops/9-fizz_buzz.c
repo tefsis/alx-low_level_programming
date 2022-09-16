a8861f09aa3eeb8c3cf265fa3db23015eaca8a47
tefsis <tefsis@gmail.com>
2022-09-16 05:51:29 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ *main - fizzbuzz interview selector
+ *Return: 0 if true
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf("Buzz");
+}
+else if (i % 15 == 0)
+{
+printf("FizzBuzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i == 100)
+{
+break;
+}
+else
+printf(" ");
+}
+printf("\n");
+return (0);
+}
