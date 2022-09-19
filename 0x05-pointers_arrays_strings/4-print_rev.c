6d854f1769665f8b1f1a81cdc5f7608624ee65c2
tefsis <tefsis@gmail.com>
2022-09-19 08:19:31 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_rev - that prints a string reversed  folloewd by a newline
+ *@str: string to be checked
+ */
+void print_rev(char *s)
+{
+int i, len, temp;
+len = strlen(s);
+for (i = 0; i < len/2; i++)  
+{
+temp = s[i];  
+s[i] = s[len - i - 1];  
+s[len - i - 1] = temp;  
+}
+_putchar(s[i]);
+}
+_putchar('\n');
+}
