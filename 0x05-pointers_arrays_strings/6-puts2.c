84757fb48072f679627df96d0e38733a74a8662f
tefsis <tefsis@gmail.com>
2022-09-19 08:46:53 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+ *puts2 - prints every character of a string starting with the first character
+ *@str: string to be checked
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i]; i++)
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
