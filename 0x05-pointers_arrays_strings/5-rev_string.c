46d673f63ae19ae48c60f09992acfcd0680cfefc
tefsis <tefsis@gmail.com>
2022-09-19 08:36:01 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,15 +1,13 @@
-include "main.h"
-/**
- *rev_string - that reverses a string
- *@s: string to be checked
- */
-void rev_string(char *s)
+int len, i, half;
+char temp;
+for (len = 0; s[len] != '\0'; len++);
+i = 0;
+half = len / 2;
+while (half--)
 {
-int i, len, temp;  
-len = strlen(s);
-{        
-temp = s[i];  
-s[i] = s[len - i - 1];  
-s[len - i - 1] = temp;  
-}  
-}  
+temp = s[len - i - 1];
+s[len - i - 1] = s[i];
+s[i] = temp;
+i++;
+}
+}
