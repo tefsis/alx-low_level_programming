57fa058d46b86024744051b4497da696c1555729
tefsis <tefsis@gmail.com>
2022-09-19 07:35:13 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *swap_int - swaps the values of two integers
+ *@a:integer one
+ *@b:integer two
+*/
+void swap_int(int *a, int *b)
+{
+int i;
+i = *a;
+*a = *b; 
+*b = i;
+}
