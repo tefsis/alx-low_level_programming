11835c0ec5b26e64dd5314cecf4ccf1b18ef09f2
tefsis <tefsis@gmail.com>
2022-09-19 05:49:50 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function creates a pointer and assign a value to it
+ *@n: a pointer variable
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
