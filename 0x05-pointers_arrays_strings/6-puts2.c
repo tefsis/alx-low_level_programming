5555dafe471c63d77b4592b18cd81784dc1243eb
tefsis <tefsis@gmail.com>
2022-09-20 07:40:40 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,14 +5,19 @@
  */
 void puts2(char *str)
 {
-  int i;
-    { 
-  for (i = 0; str[i] != '\0'; i++)
+  int i = 0;
+  int j;
+  while (str[i] != '\0')
     {
-  
-      _putchar(str[i]);
       i++;
     }
+     
+  for (j = 0; j < i ; j += 2;)
+    {
+  
+      _putchar(str[j]);
+      
+   
 
 }
 _putchar('\n');
