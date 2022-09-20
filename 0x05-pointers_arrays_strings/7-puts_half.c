0af3c31e495cf42c127a4f7d5d8f61307e01d1fe
tefsis <tefsis@gmail.com>
2022-09-20 08:14:29 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,25 +5,24 @@
  */
 void puts_half(char *str)
 {
-  int i = 0;
-  int j;
-  while (str[i] != '\0')
-    {
-      i++;
-    }
-  if (i % 2 == 0)
-    {
-  for (j = i/2; j < i ; j++)
-    {
-      _putchar(str[j]);
-    }
-    }
-  else
-    {
-      for (j = i/2 + 1; j < i ; j++)                                                                                     
-	{                                                                                                            
-	  _putchar(str[j]);
-	    }
-    }
-  _putchar('\n');
+int i = 0;
+int j;
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 == 0)
+{
+for (j = i/2; j < i ; j++)
+{
+_putchar(str[j]);
+}
+}
+else
+{
+for (j = i/2 + 1; j < i ; j++)                                                                                  {                                                                                                            
+_putchar(str[j]);
+}
+}
+_putchar('\n');
 }
