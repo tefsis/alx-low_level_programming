65f5d6eee6ddcfcaefc7c3e6e7b7c4d64fc66dc5
tefsis <tefsis@gmail.com>
2022-09-20 08:08:59 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,7 +22,7 @@ void puts_half(char *str)
     {
       for (j = i/2 + 1; j < i ; j++)                                                                                     
 	{                                                                                                            
-	  _putchar(str[j])
+	  _putchar(str[j]);
 	    }
     }
   _putchar('\n');
