a06c6bf129eafcf458daf5b3efeab1bda9ff0419
tefsis <tefsis@gmail.com>
2022-09-20 07:51:06 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *puts_half - prints half character of a string starting with the first character
+ *@str: string to be checked
+ */
+void puts_half(char *str)
+{
+  int i = 0;
+  int j;
+  while (str[i] != '\0')
+    {
+      i++;
+    }
+  for (j = i/2 + 1; j < i ; j++)
+    {
+      _putchar(str[j]);
+    }
+  _putchar('\n');
+}
