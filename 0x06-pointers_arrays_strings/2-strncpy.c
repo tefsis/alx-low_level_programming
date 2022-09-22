02b0ce0cb754ce5f234f7e5736d57f22401c23ca
tefsis <tefsis@gmail.com>
2022-09-22 05:00:31 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int j + 0;
-while (j < n && src[j] != '\0')
-{
-dest[j] = src[j];
-j++;
-}
-while (j < n)
-{
-dest[j] = '\0';
-j++;
-}
-return (dest);
-}
+  int j + 0;
+  while (j < n && src[j] != '\0')
+    {
+      dest[j] = src[j];
+      j++;
+    }
+  while (j < n)
+    {
+      dest[j] = '\0';
+      j++;
