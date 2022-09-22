38e21b375794e2ca2db6a8d862b7b1f0ca54d8ab
tefsis <tefsis@gmail.com>
2022-09-22 07:19:37 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,11 +7,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i;
+int i = 0;
 int j = 0;
 for (j = 0; (s1[j] != '\0' || s2[j] != '\0'); j++)
 {
-i = s2[j] - s1[j];
+i = *s1 - *s2;
 }
 return (i);
 }
