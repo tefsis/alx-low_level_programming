40d4a60dd2ee42472840cad28e7e16b6b505ee1d
tefsis <tefsis@gmail.com>
2022-09-23 06:53:29 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,13 +6,35 @@
  */
 char *cap_string(char *str)
 {
-int i = 0;
+int i;
 for (i = 0; str[i] != '\0'; i++)
 {
-  if (str[i] == '\t' || str[i] == ' ' || str[i] == '\n' str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' str[i] == ')' || str[i] == '{' || str[i] == '}')
-    {
-    str[i - 1] -= 32;
-    }
-    }
-  return (str);
- }
+if (i == 0)
+{
+if ((str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}
+else if (str[i] == '\t'
+|| str[i] == ' '
+|| str[i] == '\n'
+|| str[i] == ','
+|| str[i] == ';'
+|| str[i] == '.'
+|| str[i] == '!'
+|| str[i] == '?'
+|| str[i] == '"'
+|| str[i] == '('
+|| str[i] == ')'
+|| str[i] == '{'
+|| str[i] == '}')
+{
+if ((str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+{
+str[i + 1] = str[i + 1] - 32;
+}
+}
+}
+return (str);
+}
