adc64231395e17bffed7f1d73c4a48ffb5dfbcc9
tefsis <tefsis@gmail.com>
2022-09-26 12:05:15 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,18 +10,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i;
-int j;
-for (i = 0; haystack[i] != '\0'; i++)
-{
-for (j = 0; needle[j] != '\0'; j++)
-{
-if (haystack[i] == needle[j])
-{
-needle[j] = haystack[i];
-}
-}
-return (needle);
-}
-return ('\0');
+  int i;
+  int j;
+  for (i = 0; haystack[i] != '\0'; i++)
+    {
+      for (j = 0; needle[j] != '\0'; j++)
+	{
+	  if (haystack[i] != needle[j])
+	    {
+	      break;
+	    }
+	  else  if (haystack[i] == needle[j])
+	    {
+	      needle[j] = haystack[i];
+	    }
+	}
+      return (needle);
+    }
+  return ('\0');
 }
