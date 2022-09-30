22ce1cd57cbf53f574a11ea21438a28cf9c9c773
tefsis <tefsis@gmail.com>
2022-09-30 04:43:13 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - prints the number of arguments passed
+ *@argc: argument counter
+ *@argv: argument of array
+ *Return: 0 if True
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
