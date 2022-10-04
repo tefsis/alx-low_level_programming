63ca15ffd690d03bd576a2116da78065bf3fda34
tefsis <tefsis@gmail.com>
2022-10-04 09:38:45 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,39 +1,28 @@
 #include "main.h"
-#include <stdlib.h>
-
+#include  <stdlib.h>
+#include <stdio.h>
 /**
- * str_concat - concatenates two string
- *
- * @s1: the first string
- * @s2: the string to add to @s1
- *
- * Return: a pointer that points to a newly allocated space which
- * contains the contents of @s1, followed by the contents of @s2,
- * and null terminated. Should return NULL on failure
+ **str_concat - that concatenates two strings
+ *@s1: content on s1
+ *@s2: content on s2
+ *Return: NULL on failure
  */
-
 char *str_concat(char *s1, char *s2)
 {
-char *concat;
-int i = 0, j = 0, k, l = 0;
+char *concat_str;
+int i, concat_i = 0, len = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-while (s1[i])
-i++;
-while (s2[j])
-j++;
-k = i + j;
-concat = malloc(sizeof(char) * k + 1);
-if (concat == NULL)
+for (i = 0; s1[i] || s2[i]; i++)
+len++;
+concat_str = malloc(sizeof(char) * len);
+if (concat_str == NULL)
 return (NULL);
-while (l < i)
-concat[l] = s1[l];
-l++;
-while (l <k)
-concat[l] = s2[j];
-l++;
-j++;
-return (concat);
+for (i = 0; s1[i]; i++)
+concat_str[concat_i++] = s1[i];
+for (i = 0; s2[i]; i++)
+concat_str[concat_i++] = s2[i];
+return (concat_str);
 }
