63adc387cd3acc30540ff3686a831d6730d903ed
tefsis <tefsis@gmail.com>
2022-10-04 09:41:24 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,20 +9,20 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *concat_str;
-int i, concat_i = 0, len = 0;
+char *concat;
+int i, j = 0, k = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
 for (i = 0; s1[i] || s2[i]; i++)
-len++;
-concat_str = malloc(sizeof(char) * len);
-if (concat_str == NULL)
+k++;
+concat = malloc(sizeof(char) * k);
+if (concat == NULL)
 return (NULL);
 for (i = 0; s1[i]; i++)
-concat_str[concat_i++] = s1[i];
+concat[j++] = s1[i];
 for (i = 0; s2[i]; i++)
-concat_str[concat_i++] = s2[i];
-return (concat_str);
+concat[j++] = s2[i];
+return (concat);
 }
