05a86dcea95f79c939ccb8571019b675e5a145cb
tefsis <tefsis@gmail.com>
2022-10-10 09:20:53 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - that prints its name
+ *@argc: argument counter
+ *@argv: argument array
+ *Return: 0 if True
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
