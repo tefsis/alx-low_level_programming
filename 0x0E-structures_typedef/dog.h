5f5a7662771888503250aca1e658cf501c6ebfaa
tefsis <tefsis@gmail.com>
2022-10-10 13:28:06 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+/**
+ * struct dog  - dog profile
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: full dog profile
+ */
 struct dog
 {
 char *name;
