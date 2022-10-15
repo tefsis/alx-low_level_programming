443f8230cee164e519dadc16d1f1359a3227a013
tefsis <tefsis@gmail.com>
2022-10-15 05:45:57 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,10 +16,14 @@ va_list tes;
 if (separator == NULL || *separator == 0)
 return;
 va_start(tes, n);
-if (n > 0)
+for (i = 0; i < n; i++)
+{
 printf ("%d", va_arg(tes, int));
-for (i = 1; i < n; i++)
-printf("%s%d", separator, va_arg(tes, int));
+if (i < n - 1 && separator != NULL)
+{
+printf("%s", separator);
+}
+}
 printf("\n");
 va_end(tes);
 }
