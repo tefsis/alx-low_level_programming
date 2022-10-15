5c029e1f366d04f87e8ffd8bdd0719825071c483
tefsis <tefsis@gmail.com>
2022-10-15 05:52:54 -0700

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+
+/**
+ * print_strings - prints strings
+ *
+ * @separator: character who separate number
+ * @n: element's string
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list list;
+char *argument;
+va_start(list, n);
+for (i = 0; i < n; i++)
+{
+argument = va_arg(list, char *);
+if (argument == NULL)
+printf("%s", "(nil)");
+else
+printf("%s", argument);
+if (i < n - 1 && separator != NULL)
+{
+printf("%s", separator);
+}
+}
+va_end(list);
+printf("\n");
+}
