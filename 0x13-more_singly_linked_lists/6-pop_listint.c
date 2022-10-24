60c6d0b98b904b29234e24f1e4e0e1dce5d482ca
tefsis <tefsis@gmail.com>
2022-10-24 07:20:53 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint -  deletes the first node
+ * @head: Parameter with the head node
+ * Return: Returns nothing
+ */
+int pop_listint(listint_t **head)
+{
+listint_t *to_be_deleted;
+if (*head == NULL)
+{
+return (0);
+}
+while (*head != NULL)
+{
+to_be_deleted = *head;
+*head = to_be_deleted->next;
+free(to_be_deleted);
+}
+return (0);
+}
