943b459a511c05ffb8706bae0b436300bd1d0e35
tefsis <tefsis@gmail.com>
2022-10-24 08:07:54 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_list -  prints all the elements of a listint_t list.
+ * print_listint -  prints all the elements of a listint_t list.
  *
  * @h: points to the next node
  * Return: the number of nodes
