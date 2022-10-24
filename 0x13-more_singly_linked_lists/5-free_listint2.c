13ee4c48a16145f999284679bd44c248296449ae
tefsis <tefsis@gmail.com>
2022-10-24 06:45:43 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint -  frees a list_t list
+ * @head: Parameter with the head node
+ * Return: Returns nothing
+ */
+void free_listint(listint_t *head)
+{
+listint_t *cleaner;
+while (head != NULL)
+{
+cleaner = head;
+head = head->next;
+free(cleaner);
+}
+head = NULL;
+}
