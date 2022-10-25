e07c456887ea78c74f7b75d9c7b1ab9c6f122dd4
tefsis <tefsis@gmail.com>
2022-10-25 04:40:03 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -15,8 +15,8 @@ return (0);
 while ((*head) != NULL)
 {
 to_be_deleted = (*head);
-n =to_be_deleted->n;
-(*head) = to_be_deleted->next;
+n = to_be_deleted->n;
+head = to_be_deleted->next;
 free(to_be_deleted);
 }
 return (n);
