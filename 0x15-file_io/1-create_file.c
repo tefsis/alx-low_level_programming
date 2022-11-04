71ca02ab5c4b66e3eea9eb47ac54132d10f7f889
tefsis <tefsis@gmail.com>
2022-11-04 07:34:06 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,7 @@ return (-1);
 if (!text_content)
 text_content = "";
 for (nletters = 0; text_content[nletters]; nletters++)
+;
 wt = write(fd, text_content, nletters);
 if (wt == -1)
 return (-1);
